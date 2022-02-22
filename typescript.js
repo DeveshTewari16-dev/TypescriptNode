8cf3e2a451aacf3612ac97f146c8767856a3b790
Tewari <devtewari@deloitte.com>
2022-02-22 12:00:10 +0530

    Typescript with node js code

---
+++ typescript.js
@@ -0,0 +1,6 @@
+var person = {
+    name: 'Devesh',
+    age: 24
+};
+console.log(person.name);
+//Object Type can be used in Typescript to basically let the type script know that how it should percieve the object
---
+++ typescript.ts
@@ -0,0 +1,13 @@
+const person: {
+    name : string,
+    age:number,
+    hobbies: string[]
+} = {
+    name: 'Devesh',
+    age:24,
+    hobbies :['Basketball','Guitar']
+}
+
+console.log(person.name);
+
+ //Object Type can be used in Typescript to basically let the type script know that how it should percieve the object
